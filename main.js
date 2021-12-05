a1f589481fab283c466e978540df20efcd442333
Aaron Lee <arnnlee5@gmail.com>
2021-12-05 10:19:47 -0800

    [append-div] - Append to parent div instead of body

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 function doTheRestOfTheStuff() {
-const isPhantomInstalled = window.solana && window.solana.isPhantom
-let btn = document.createElement("button");
-if(isPhantomInstalled == true) {
-  btn.innerHTML = "Connect to Wallet";
-  btn.onclick = connectWallet;
-}else{
-  window.alert("Solana object not found! Get a Phantom Wallet 👻");
-}
-document.body.appendChild(btn);
+  const isPhantomInstalled = window.solana && window.solana.isPhantom
+  const div = document.getElementById("root");
+  let btn = document.createElement("button");
+  if(isPhantomInstalled == true) {
+    btn.innerHTML = "Connect to Wallet";
+    btn.onclick = connectWallet;
+  } else {
+    window.alert("Solana object not found! Get a Phantom Wallet 👻");
+  }
+  div.appendChild(btn);
 }
 
 window.addEventListener('load', doTheRestOfTheStuff)
 
-
 async function connectWallet() {
   try {
     const resp = await window.solana.connect();
-} catch (err) {}
+  } catch (err) {}
 };
-
-
